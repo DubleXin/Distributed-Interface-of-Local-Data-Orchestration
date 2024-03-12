3acb9d65fb993adb99fe03e43cbb117618305b62
DubleXin <progames720.2@gmail.com>
2024-03-12 17:37:22 +0100

    CLientModel implementation

--- DILDO/DILDO Library/src/Client/Net/ClientState.cs
+++ DILDO/DILDO Library/src/Client/Net/ClientState.cs
@@ -3,43 +3,27 @@ using DILDO.net.IO;
 using DILDO.server.core.factories;
 using static DILDO.server.controllers.PacketRouter;
 
-using System.Collections.Concurrent;
 using System.Net;
-using System.Net.Sockets;
 using System.Text;
 
 using ServerModel = DILDO.server.models.ServerModel;
-using DILDO.server;
-
 namespace DILDO.client;
+
 public class ClientState : StateProfile, IDisposable
 {
     public static ClientState? Instance { get; private set; }
 
-    private UdpClient? _sendClient;
-    private UdpClient? _receiveClient;
-    public Action? OnUserConnectEvent;
-    public Action? OnUserDisconnectEvent;
-    public Action<(Guid, string)[]>? OnServerAddressFound;
-
-    private ConcurrentDictionary<Guid, string>? _serverNames;
-    private ConcurrentDictionary<Guid, Guid>? _serverConnectInfo;
-    private ConcurrentDictionary<Guid, UDPPacket>? _receivedPackets;
-
-    private Guid _id;
-
-    private CancellationTokenSource? _cts;
+    private ClientModel _model;
 
     public ClientState() : base()
     {
-        _id = Guid.NewGuid();
-
+        _model = new();
         Instance = this;
     }
 
     public (Guid, string)[] GetServers()
     {
-        var servers = _serverNames.ToArray();
+        var servers = _model.ServerNames.ToArray();
         List<(Guid, string)> listToInvoke = new();
         foreach (var server in servers)
             listToInvoke.Add((server.Key, server.Value));
@@ -48,14 +32,6 @@ public class ClientState : StateProfile, IDisposable
 
     public override void Launch()
     {
-        _cts = new CancellationTokenSource();
-        _receiveClient = new UdpClient(ServerModel.DEFAULT_SERVER_SEND_PORT);
-        _sendClient = new UdpClient();
-
-        _serverNames = new ConcurrentDictionary<Guid, string>();
-        _receivedPackets = new();
-        _serverConnectInfo = new();
-
         Debug.Log<ClientState>($"<WHI> Client Started.");
 
         ListenToConnection();
@@ -63,15 +39,15 @@ public class ClientState : StateProfile, IDisposable
 
     private void ListenToConnection()
     {
-        _receiveClient.EnableBroadcast = true;
+        _model.ReceiveCLient.EnableBroadcast = true;
         Task.Run(() =>
         {
             var endpoint = new IPEndPoint(IPAddress.Any, 0);
-            while(!_cts.IsCancellationRequested)
+            while(!_model.CancellationToken.IsCancellationRequested)
             {
                 try
                 {
-                    byte[] message = _receiveClient.Receive(ref endpoint);
+                    byte[] message = _model.ReceiveCLient.Receive(ref endpoint);
                     string encoded = Encoding.UTF32.GetString(message);
 
                     PacketReaderBroker reader = new(encoded);
@@ -93,7 +69,7 @@ public class ClientState : StateProfile, IDisposable
                 }
                 catch { }
             }
-            _cts.Dispose();
+            _model.CancellationToken.Dispose();
             Dispose();
         });
     }
@@ -101,23 +77,23 @@ public class ClientState : StateProfile, IDisposable
     {
         var raw = packet.RawData;
         if (Guid.TryParse(raw[2], out var guid))
-            if (_serverNames.TryAdd(guid, raw[3]) && _serverConnectInfo.TryAdd(guid, packet.ConfirmID))
+            if (_model.ServerNames.TryAdd(guid, raw[3]) && _model.ServerConnectInfo.TryAdd(guid, packet.ConfirmID))
             {
-                var servers = _serverNames.ToArray();
+                var servers = _model.ServerNames.ToArray();
                 List<(Guid, string)> listToInvoke = new();
                 foreach (var server in servers)
                     listToInvoke.Add((server.Key, server.Value));
-                OnServerAddressFound?.Invoke(listToInvoke.ToArray());
+                _model.OnServerAddressFound?.Invoke(listToInvoke.ToArray());
             }
     }
     private void TryAddPacketToList(UDPPacket packet)
     {
-        if (_receivedPackets.TryAdd(packet.ID, packet))
+        if (_model.ReceivedPackets.TryAdd(packet.ID, packet))
             RaisePacketReceiver(packet, "private void TryAddPacketToList(UDPPacket packet)");
     }
     public void ConnectToServer(Guid guid)
     {
-        if(!_serverConnectInfo.TryGetValue(guid, out var info))
+        if(!_model.ServerConnectInfo.TryGetValue(guid, out var info))
             return;
         var sendingData = PacketFactory.GetFactory
                 (OpCode.BroadcastStringMessage).GetPacket(new string[]
@@ -126,7 +102,7 @@ public class ClientState : StateProfile, IDisposable
                     ((int)PacketType.SessionConfirm).ToString(),
                     guid.ToString(),
                     info.ToString(),
-                    _id.ToString(),
+                    _model.ID.ToString(),
                     Guid.NewGuid().ToString()
                 }).Data;
 
@@ -135,22 +111,22 @@ public class ClientState : StateProfile, IDisposable
 
         var endpoint = new IPEndPoint
         (IPAddress.Broadcast, ServerModel.DEFAULT_SERVER_RECEIVE_PORT);
-        _sendClient.Send(sendingData, sendingData.Length, endpoint);
-        Debug.Log<ClientState>($"<DMA> Requesting connection to server named: <MAG>{_serverNames[guid]}");
+        _model.SendClient.Send(sendingData, sendingData.Length, endpoint);
+        Debug.Log<ClientState>($"<DMA> Requesting connection to server named: <MAG>{_model.ServerNames[guid]}");
     }
 
     public override void Close() 
     { 
-        _cts.Cancel(); 
-        _receiveClient.Close(); 
+        _model.CancellationToken.Cancel(); 
+        _model.ReceiveCLient.Close(); 
     }
 
     public void Dispose()
     {
-        _sendClient.Close();
+        _model.SendClient.Close();
 
-        _receiveClient.Dispose();
-        _sendClient.Dispose();
+        _model.ReceiveCLient.Dispose();
+        _model.SendClient.Dispose();
 
         Debug.Log<ClientState>("<DRE> Client Closed and Disposed.");
         StateBroker.Instance.OnStateClosed?.Invoke();
--- DILDO/DILDO Library/src/Server/Models/ClientModel.cs
+++ DILDO/DILDO Library/src/Server/Models/ClientModel.cs
@@ -1,11 +1,37 @@
-﻿
+﻿using System.Collections.Concurrent;
+using System.Net.Sockets;
+
 namespace DILDO.server.models
 {
     public class ClientModel
     {
-        public string? Name { get; set; }
-        public Guid ID { get; set; }
-        public ushort Port { get; set; }
+        public UdpClient SendClient { get; set; }
+        public UdpClient ReceiveCLient { get; set; }
 
+        public Action? OnUserConnectEvent;
+        public Action? OnUserDisconnectEvent;
+        public Action<(Guid, string)[]>? OnServerAddressFound;
+
+        public ConcurrentDictionary<Guid, string>? ServerNames { get; set; }
+        public ConcurrentDictionary<Guid, Guid>? ServerConnectInfo { get; set; }
+        public ConcurrentDictionary<Guid, UDPPacket>? ReceivedPackets { get; set; }
+
+        public Guid ID { get; set; }
+
+        public CancellationTokenSource CancellationToken { get; set; }
+
+        public ClientModel()
+        {
+            SendClient = new();
+            ReceiveCLient = new();
+
+            ServerNames = new();
+            ServerConnectInfo = new();
+            ReceivedPackets = new();
+
+            ID = Guid.NewGuid();
+
+            CancellationToken = new();
+        }
     }
 }
