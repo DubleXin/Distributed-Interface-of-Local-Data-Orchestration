522867738c591acbfc2e5a1263e5ce7f18b7ee7b
DubleXin <progames720.2@gmail.com>
2024-03-12 17:56:31 +0100

    fix

--- DILDO/DILDO Library/src/Server/Models/ClientModel.cs
+++ DILDO/DILDO Library/src/Server/Models/ClientModel.cs
@@ -37,7 +37,6 @@ namespace DILDO.server.models
         public void Dispose()
         {
             Dispose(true);
-            GC.SuppressFinalize(this);
         }
 
         protected virtual void Dispose(bool disposing)
