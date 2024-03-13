3208543f3c493bee01965b8db3b44d7968e45aba
Kirserd <87417121+Kirserd@users.noreply.github.com>
2024-03-14 00:20:44 +0100

    FreePort Find Util

---
+++ DILDO/DILDO Library/src/Utils/FreePort.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace DILDO.src.Utils
+{
+    public static class FreePort
+    {
+        public static int Find()
+        {
+            int port = 0;
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 0);
+                socket.Bind(localEP);
+                localEP = (IPEndPoint)socket.LocalEndPoint;
+                port = localEP.Port;
+            }
+            finally
+            {
+                socket.Close();
+            }
+            return port;
+        }
+    }
+}
