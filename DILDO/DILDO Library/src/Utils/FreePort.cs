0ac1b615d51e87aeff5d399bb73ba75ab4381115
Kirserd <87417121+Kirserd@users.noreply.github.com>
2024-03-14 00:21:56 +0100

    patch

--- DILDO/DILDO Library/src/Utils/FreePort.cs
+++ DILDO/DILDO Library/src/Utils/FreePort.cs
@@ -1,26 +1,42 @@
 ﻿using System.Net.Sockets;
 using System.Net;
+using System.Net.NetworkInformation;
 
 namespace DILDO.src.Utils
 {
     public static class FreePort
     {
-        public static int Find()
+        public static int Find(int startingPort)
         {
-            int port = 0;
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 0);
-                socket.Bind(localEP);
-                localEP = (IPEndPoint)socket.LocalEndPoint;
-                port = localEP.Port;
-            }
-            finally
-            {
-                socket.Close();
-            }
-            return port;
+            var portArray = new List<int>();
+
+            var properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            // Ignore active connections
+            var connections = properties.GetActiveTcpConnections();
+            portArray.AddRange(from n in connections
+                               where n.LocalEndPoint.Port >= startingPort
+                               select n.LocalEndPoint.Port);
+
+            // Ignore active tcp listners
+            var endPoints = properties.GetActiveTcpListeners();
+            portArray.AddRange(from n in endPoints
+                               where n.Port >= startingPort
+                               select n.Port);
+
+            // Ignore active UDP listeners
+            endPoints = properties.GetActiveUdpListeners();
+            portArray.AddRange(from n in endPoints
+                               where n.Port >= startingPort
+                               select n.Port);
+
+            portArray.Sort();
+
+            for (var i = startingPort; i < UInt16.MaxValue; i++)
+                if (!portArray.Contains(i))
+                    return i;
+
+            return 0;
         }
     }
 }
